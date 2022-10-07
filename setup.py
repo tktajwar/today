c2ae25d215bb646b948c0f97a73411b69b484995
TajwarHjkl <Tajwar.Earth@pm.me>
2022-10-07 22:30:21 +0600

    make project pypi ready

--- setup.py
+++ setup.py
@@ -1,15 +1,23 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
         name='today_cli',
-        version='1.0',
-        description='Plan and execute your day in an organised way.',
-        url='https://github.com/TajwarHjkl/today',
+        version='1.0.0',
+        description='Command Line Day Planner',
+        long_description = long_description,
+        long_description_content_type="text/markdown",
+        url='https://github.com/TajwarHjkl/today/archive/refs/tags/v1.0.0.tar.gz',
         author='Takey Tajwar',
         author_email='tajwar.earth@proton.me',
         license='GPLv3',
         packages=['today'],
         scripts=['bin/today'],
         package_data={'today': ['themes/*.json']},
-        zip_safe=False,
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "Operating System :: OS Independent",
+    ],
         )
