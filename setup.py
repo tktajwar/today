199bf4a63e8d021be2bd464057cc163737afe2a6
TajwarHjkl <Tajwar.Earth@pm.me>
2022-10-12 15:51:31 +0600

    update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
         name='today_cli',
-        version='1.1.0',
+        version='1.2.0',
         description='Command Line Day Planner',
         long_description = long_description,
         long_description_content_type="text/markdown",
